257e6d190e1abfaac46b22463dceb20dca9383b4
augustob790 <augustosousa790@gmail.com>
2020-01-06 22:11:21 -0300

    App lista de tarefas em Flutter

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:app_listastarefas/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+  debugShowCheckedModeBanner: false,
+));
+
